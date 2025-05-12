0d470e06a54bab12500f062a4957ddb51e738649
BenMasse15 <ben.masse@icloud.com>
2025-05-12 13:25:23 -0400

    Add files via upload

---
+++ src/DEL_Control.cpp
@@ -0,0 +1,72 @@
+// #include <Adafruit_NeoPixel.h>
+// #ifdef __AVR__
+//   #include <avr/power.h>
+// #endif
+// #define PIN       16
+// #define NUMPIXELS 16
+
+// Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+// #define DELAYVAL 500
+
+// void setup() {
+//   pixels.begin();
+//   pixels.show();
+//   pixels.setPin(16);
+//   pixels.setBrightness(255);
+// }
+
+// void loop() {
+//   pixels.setPixelColor(0,pixels.Color(255,0,0));
+  
+// }
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN       1
+#define NUMPIXELS 10
+void Set_Color_Red (void);
+void Set_Color_Yellow(void);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
+
+// void loop() {
+//   Set_Color_Red();
+//   delay(1000);
+//   Set_Color_Yellow();
+//   delay(1000);
+// }
+
+
+void Set_Color_Red (uint16_t Outside_Brightness){
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+  #endif
+  pixels.setPin(1);
+  pixels.begin();
+  pixels.setBrightness(255 * Outside_Brightness / 19456); // Maximum LED Brightness multiplied by the input value, divided by the maximum input value
+  pixels.clear();
+
+  for(int i=0; i<NUMPIXELS; i++) {
+
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+    pixels.show();
+  }
+}
+
+void Set_Color_Yellow(uint16_t Outside_Brightness){
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+  #endif
+  pixels.setPin(1);
+  pixels.begin();
+  pixels.setBrightness(255 * Outside_Brightness / 19456); // Maximum LED Brightness multiplied by the input value, divided by the maximum input value
+  pixels.clear();
+
+  for(int i=0; i<NUMPIXELS; i++) {
+
+    pixels.setPixelColor(i, pixels.Color(255, 255, 0));
+    pixels.show();
+  }
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include <Arduino.h>
 #include "affichage.h"
+#include <DEL_Control.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  
 }
+
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for 1second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                      // wait for 100ms
+  pinMode(5, INPUT);
+  
+  if((digitalRead(5)) == 0){
+    Set_Color_Red(511);
+  }
+  if((digitalRead(5)) == 1){
+    Set_Color_Yellow(511);   
+  }
 }
